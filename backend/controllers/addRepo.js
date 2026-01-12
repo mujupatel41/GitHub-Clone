fc250639bb8ee0be67262f413be8531bec4dabc2
mujupatel41 <mujupatel41@gmail.com>
2026-01-12 12:06:34 +0530

    function is created for an addRepo.js to add a file with run command and it will go to stating area

--- backend/controllers/addRepo.js
+++ backend/controllers/addRepo.js
@@ -1,5 +1,19 @@
-async function addRepo() {
-    console.log("Added Successfully")
-}
+const fs = require("fs").promises;
+const path = require("path");
+
+async function addRepo(filePath) {
+    const repoPath = path.resolve(process.cwd(), ".myGit");
+    const stagingPath = path.join(repoPath, "staging");
+
+    try{
+        await fs.mkdir(stagingPath, {recursive: true});
+        const fileName = path.basename(filePath);
+        await fs.copyFile(filePath, path.join(stagingPath, fileName));
+        console.log(`File ${fileName} is added to staging area`);
+    } catch(err) {
+        console.error("Error Adding File: ", err.message);
+    }
+    
+};
 
 module.exports = {addRepo}
--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,9 @@ yargs(hideBin(process.argv))
             type: "string"
         });
     },
-    addRepo
+    (argv)=>{
+        addRepo(argv.file)
+    }
 ).command(
     "commit <message>",
     "Commit the staged file",
