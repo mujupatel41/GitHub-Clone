fdc19b2ddca58e7052319b7526e9b3c8a9f3d7ef
mujupatel41 <mujupatel41@gmail.com>
2026-01-16 10:13:35 +0530

    config some errors in aws and build the logic in pushRepo.js

--- backend/config/aws-config.js
+++ backend/config/aws-config.js
@@ -1,8 +1,13 @@
+require("dotenv").config();
 const AWS = require("aws-sdk");
 
 AWS.config.update({region: "eu-north-1"});
 
-const s3 = AWS.S3();
+const s3 = new AWS.S3({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: "eu-north-1"
+});
 
 const S3_BUCKET = "mygitbucket41";
 
