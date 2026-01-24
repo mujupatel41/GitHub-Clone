57f30140dbf6ad56d1e92779ecd6b2e4b32e5e0a
mujupatel41 <mujupatel41@gmail.com>
2026-01-24 12:52:18 +0530

    routes and controller make for issue

---
+++ backend/controllers/issueController.js
@@ -0,0 +1,27 @@
+const createIssue = (req, res) =>{
+    res.send("Issue Created!");
+};
+
+const updateIssueById = (req, res) =>{
+    res.send("Issue Updated!");
+};
+
+const deleteIssueById = (req, res) =>{
+    res.send("Issue Deleted!");
+};
+
+const getAllIssues = (req, res) =>{
+    res.send("All Issues Fetched!");
+};
+
+const getIssueById = (req, res) =>{
+    res.send("Issue Details Fetched!");
+};
+
+module.exports = {
+    createIssue,
+    updateIssueById,
+    deleteIssueById,
+    getAllIssues,
+    getIssueById,
+};
