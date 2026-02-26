3cc2e5cc4211c991610cd70a20073c85285d4367
mujupatel41 <mujupatel41@gmail.com>
2026-02-26 07:57:32 +0530

    worked on Routes

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -1,9 +1,44 @@
-import React from "react";
+import React, {useState, useEffact} from 'react';
+import axios from 'axios';
+import {useAuth} from '../../authContext';
+
+import {PageHeader} from '@primer/react/drafts';
+import { Box, Button } from '@primer/react';
+import "./auth.css";
+
+import Logo from '../../assets/github-icon.svg'
 
 const Login = () =>{
-    return <>
-        <h1>Login</h1>
-    </>
+
+    useEffect(()=>{
+        localStorage.removeItem("token");
+        localStorage.removeItem("userId");
+        setCurrentUser(null);
+    });
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [Loading, setLoading] = useState(false);
+    const {currentUser, setCurrentUser} = useAuth();
+
+
+    return (
+        <div className="login-wraper">
+            <div className="login-logo-container">
+                <img className='logo-login' src={Logo} alt="github-logo" />
+            </div>
+
+            <div className="login-box-wrapper">
+                <div className="login-heading">
+                    <Box sx={{ padding: 1 }}>
+                        <PageHeader>
+                            
+                        </PageHeader>
+                    </Box>
+                </div>
+            </div>
+        </div>
+    )
 };
 
 export default Login;
